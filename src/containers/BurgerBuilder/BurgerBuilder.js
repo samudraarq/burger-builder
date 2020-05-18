a66d07d69854f3bb33bb61197110f7de3eb635a3
Samudra Faris Arqam <>
2020-05-18 17:00:21 +0700

    first commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import Aux from "../../hoc/Aux";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>Burgers</div>
+        <div>BuildControls</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
